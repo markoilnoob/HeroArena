f755acc4bf9b10dee1e1e5a06aa7181bb17b3079
Lodo Marco <marco.lodo1010@gmail.com>
2025-03-26 15:59:22 +0100

    Created Enum HeroClass

---
+++ Assets/Code/Data/HeroArenaType.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace HeroArena
+{
+    public enum HeroClass
+    {
+        NONE,
+        BARBARIAN,
+        PALADIN,
+        ROGUE,
+        RANGER
+    }
+}
--- Assets/Code/GameState.cs
+++ Assets/Code/GameState.cs
@@ -4,6 +4,36 @@ namespace HeroArena
 {
     public class GameState : MonoBehaviour
     {
+        public static GameState Instance { get; private set; }
+
+        private HeroClass heroSelected;
+        public HeroClass HeroSelected
+        {
+            get
+            {
+                return heroSelected;
+            }
+            private set
+            {
+                heroSelected = value;
+            }
+        }
+
+        //Singleton
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+
+            heroSelected = HeroClass.NONE;
+        }
+
 
     }
 }
